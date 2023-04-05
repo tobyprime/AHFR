305352a0fab8b2d1ad7a10791636014d22ced5c9
tobyprime <tobyprime0516@outlook.com>
2023-04-05 13:55:18 +0800

    add:allow tag attributes to be ignored when reading xml, simplifying reading

--- parser.py
+++ parser.py
@@ -3,7 +3,7 @@ from type import *
 from xml.etree import ElementTree
 
 
-def JsonParser(container: type|dict, data):
+def JsonParser(container: type | dict, data):
     data = json.loads(data)
     return ValueParser(container, data)
 
@@ -55,28 +55,28 @@ def ValueParser(container: type, value: dict):
     return value
 
 
-def element2dict(xml):
+def element2dict(xml, ignore_attr=False):
     attr = dict(xml.attrib)
     value = {}
     if len(xml) == 0:
-        return {
+        return xml.text if ignore_attr else {
             "attr": attr,
             "value": xml.text
         }
     for element in xml:
         if element.tag in value.keys():
             if isinstance(value[element.tag], list):
-                value[element.tag].append(element2dict(element))
+                value[element.tag].append(element2dict(element, ignore_attr))
             else:
-                value[element.tag] = [value[element.tag], element2dict(element)]
+                value[element.tag] = [value[element.tag], element2dict(element, ignore_attr)]
             continue
-        value[element.tag] = element2dict(element)
-    return {
+        value[element.tag] = element2dict(element, ignore_attr)
+    return value if ignore_attr else {
         "attr": attr,
         "value": value,
     }
 
 
-def XmlParser(container: type, data):
-    data = element2dict(data.getroot())
+def XmlParser(container: type, data,ignore_attr=False):
+    data = element2dict(data.getroot(),ignore_attr)
     return ValueParser(container, data)
