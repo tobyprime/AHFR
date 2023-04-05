ce893f062ed4a336555ff9fa40a4d3153f6282a4
tobyprime <tobyprime0516@outlook.com>
2023-04-05 12:00:50 +0800

    Initial commit

---
+++ parser.py
@@ -0,0 +1,76 @@
+import json
+from type import *
+from xml.etree import ElementTree
+
+
+def JsonParser(container: type, data):
+    data = json.loads(data)
+    return ValueParser(container, data)
+
+
+def ValueParser(container: type, value: dict):
+    if check_sub_type(container, Template):
+        result = container()
+        for k, v in container.__annotations__.items():
+            if k[0] == "_":
+                continue
+
+            if check_any_type(v):
+                result.__setattr__(k, value[k])
+                continue
+
+            if check_template_type(v):
+                result.__setattr__(k, ValueParser(v, value[k]))
+                continue
+
+            true_type = check_list_type(v)
+            if true_type:
+                result.__setattr__(k, [ValueParser(true_type, i) for i in value[k]])
+                continue
+
+            true_type = check_optional_type(v)
+            if true_type:
+                result.__setattr__(k, ValueParser(true_type, value[k]) if k in value.keys() else None)
+                continue
+
+            getter_setter = check_getter_setter_type(v)
+            if getter_setter:
+                getter = getter_setter[0]
+                result.__setattr__(k, getter(value[k]))
+                continue
+
+            if check_type(value[k], v):
+                result.__setattr__(k, value[k])
+                continue
+
+            else:
+                result.__setattr__(k, v(value[k]))
+        return result
+    return value
+
+
+def element2dict(xml):
+    attr = dict(xml.attrib)
+    value = {}
+    if len(xml) == 0:
+        return {
+            "attr": attr,
+            "value": xml.text
+        }
+    for element in xml:
+        if element.tag in value.keys():
+            if isinstance(value[element.tag], list):
+                value[element.tag].append(element2dict(element))
+            else:
+                value[element.tag] = [value[element.tag], element2dict(element)]
+            continue
+        value[element.tag] = element2dict(element)
+    return {
+        "attr": attr,
+        "value": value,
+    }
+
+
+def XmlParser(container: type, data):
+    data = element2dict(data.getroot())
+    return ValueParser(container, data)
---
+++ type.py
@@ -0,0 +1,123 @@
+import typing
+from abc import ABC
+
+from types import NoneType, GenericAlias, UnionType
+from typing import Union, Callable, Optional, Any, List
+from typing import _GenericAlias, _UnionGenericAlias
+
+
+class Template(ABC):
+    def to_dict(self):
+        return vars(self)
+
+    def __repr__(self):
+        return str(self.to_dict())
+
+
+class XMLElementTemplate(Template):
+    attr: Optional[dict]
+    value: ...
+
+    def __getitem__(self, item):
+        return self.attr[item]
+
+    def __call__(self, *args, **kwargs):
+        return self.value
+
+
+class SpecialForm:
+    def __init__(self, getitem):
+        self._getitem = getitem
+        self._name = getitem.__name__
+        self.__doc__ = getitem.__doc__
+
+    def __getitem__(self, item):
+        return self._getitem(*item)
+
+
+@SpecialForm
+def GetterSetter(getter: Callable, setter: Callable):
+    return Union[getter, setter]
+
+
+@SpecialForm
+def XmlElement(attr_type, value_type):
+    class T(XMLElementTemplate):
+        attr: attr_type
+        value: value_type
+
+    return T
+
+
+def check_type(v, t):
+    try:
+        return isinstance(v, t)
+    except:
+        return False
+
+
+def check_sub_type(t, tp):
+    try:
+        return issubclass(t, tp)
+    except:
+        return False
+
+
+def check_generic_alias_type(t):
+    if check_type(t, GenericAlias) or check_type(t, _GenericAlias):
+        return t.__origin__, t.__args__
+    return False
+
+
+def check_list_type(t):
+    """
+    List[T],list[T]
+    :return: T
+    """
+    result = check_generic_alias_type(t)
+    if result:
+        origin, args = result
+        if origin == list and len(args) == 1:
+            return args[0]
+    return False
+
+
+def check_optional_type(t):
+    """
+    Optional[T],T|None,T|NoneType
+    :return: T
+    """
+    result = check_generic_alias_type(t)
+    if result:
+        origin, args = result
+        if origin == Union and len(args) == 2 and args[1] == NoneType:
+            return args[0]
+    if check_type(t, UnionType):
+        args = t.__args__
+        if len(args) == 2 and (args[1] == NoneType or args[1] is None):
+            return args[0]
+    return False
+
+
+def check_any_type(t):
+    """
+    None,NoneType,Any,...
+    :return: True or False
+    """
+    return t is None or t is NoneType or t is Any or t is Ellipsis
+
+
+def check_template_type(t):
+    return check_sub_type(t, Template)
+
+
+def check_getter_setter_type(t):
+    """
+    GetterSetter[getter,setter]
+    :return: getter,setter
+    """
+    if check_type(t, UnionType):
+        args = t.__args__
+        if len(args) == 2 and check_type(args[0], Callable) and check_type(args[1], Callable):
+            return args[0], args[1]
+    return False
